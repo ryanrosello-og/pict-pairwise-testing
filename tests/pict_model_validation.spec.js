7e5dbcbe850306068fa03a929db1d85ac60fdd91
ryanrosello-og <goneby30+github@gmail.com>
2021-06-09 06:11:14 +1000

    catch error thrown from PICT

--- tests/pict_model_validation.spec.js
+++ tests/pict_model_validation.spec.js
@@ -2,45 +2,26 @@ import Pict from '../lib/pict.js';
 import assert from 'assert';
 
 describe('pict - model validation', () => {
-  let pict;
-  let model;
+  it('constraints must end with a ; character', () => {
+    let pict = new Pict({
+      parameters: [
+        { property: 'Size', values: [10, 100, 500, 1000, 5000, 10000, 40000] },
+        { property: 'Format method', values: ['quick', 'slow'] },
+        { property: 'Format method', values: ['quick', 'slow'] },
+        { property: 'File system', values: ['FAT', 'FAT32', 'NTFS'] },
+      ],
+    });
 
-  const jsonModel = {
-    parameters: [
-      {
-        property: 'Type',
-        values: [
-          'Primary',
-          'Logical',
-          'Single',
-          'Span',
-          'Stripe',
-          'Mirror',
-          'RAID-5',
-        ],
-      },
-      { property: 'Size', values: [10, 100, 500, 1000, 5000, 10000, 40000] },
-      { property: 'Format method', values: ['quick', 'slow'] },
-      { property: 'File system', values: ['FAT', 'FAT32', 'NTFS'] },
-      {
-        property: 'Cluster size',
-        values: [512, 1024, 2048, 4096, 8192, 16384, 32768, 65536],
-      },
-      { property: 'Compression', values: ['on', 'off'] },
-    ],
-    constraints: [
-      'IF [File system] = "FAT"   THEN [Size] <= 4096;',
-      'IF [File system] = "FAT32" THEN [Size] <= 32000;',
-    ],
-  };
-
-  before(() => {
-    pict = new Pict(jsonModel);
+    let result = pict.generateTestCases();
+    assert.match(
+      result.command,
+      /Command failed: .\/binaries\/pict_nix.*_generated.model/
+    );
+    assert.strictEqual(
+      result.error,
+      'Input Error: A parameter names must be unique'
+    );
   });
-
-  it('constraints must end with a ; character');
   it('values can either be string or numeric');
   it('values cannot be empty');
-
-  
 });
