69c4d6f553a6251aae50799048149eb582460441
ryanrosello-og <goneby30+github@gmail.com>
2021-06-10 05:43:03 +1000

    file based pict file

--- tests/pict_support_model_types.spec.js
+++ tests/pict_support_model_types.spec.js
@@ -1,5 +1,6 @@
 import Pict from '../lib/pict.js';
 import assert from 'assert';
+import path from 'path';
 
 describe('pict - describe model as text or from file', () => {
   it('can execute text based model', () => {
@@ -8,6 +9,23 @@ describe('pict - describe model as text or from file', () => {
         Threshold: 10,100,500
         Approved: Yes, No
     `);
+
+    let model = pict.generateTestCases();
+    assert.deepStrictEqual(model.testCases, [
+      { status: 'Open', threshold: '100', approved: 'No' },
+      { status: 'Closed', threshold: '10', approved: 'No' },
+      { status: 'Closed', threshold: '500', approved: 'Yes' },
+      { status: 'Closed', threshold: '100', approved: 'Yes' },
+      { status: 'Open', threshold: '10', approved: 'Yes' },
+      { status: 'Open', threshold: '500', approved: 'No' },
+    ]);
+  });
+
+  it('can generate model from static file', () => {
+    const __dirname = path.resolve(path.dirname(''));
+    const modelFile = path.resolve(__dirname, 'fixtures/test-based.model');
+
+    let pict = new Pict(modelFile);
 
     let model = pict.generateTestCases();
     assert.deepStrictEqual(model.testCases, [
